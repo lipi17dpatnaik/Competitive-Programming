53605ec67db995bd98b3c3d981b4b9c1d85631e1
Lipi Deepaakshi Patnaik <40713338+lipi17dpatnaik@users.noreply.github.com>
2019-01-07 13:55:55 +0530

    Create Kadane's Algorithm.cpp

---
+++ Kadane's Algorithm.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+  
+int MSAS(int a[], int size) 
+//making function Maximum Sub-array Sum
+{ 
+   int final_max = a[0]; 
+   int curr_max = a[0]; 
+   int i;
+  
+   for (i = 1; i < size; i++) 
+   { 
+        curr_max = max(a[i], curr_max+a[i]); 
+        final_max  = max(final_max, curr_max); 
+   } 
+   return final_max ; 
+} 
+  
+// Test Maximum Sub-array Sum
+int main() 
+{ 
+   int a[] =  {4, -1, -2, 1, 5, -3}; 
+   int n = sizeof(a)/sizeof(a[0]); 
+   int max_sum = MSAS(a, n); 
+   std::cout << "Maximum contiguous sum is " << max_sum; 
+   return 0; 
+} 
