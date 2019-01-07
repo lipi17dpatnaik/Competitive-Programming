d78e6e4f780eb7fbdd4140705530aa3e93cce0fb
Lipi Deepaakshi Patnaik <40713338+lipi17dpatnaik@users.noreply.github.com>
2019-01-07 14:59:42 +0530

    Create Missing Number in Array.cpp

---
+++ Missing Number in Array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace
+  
+int MissingNum(int a[])
+{ int sum=0;
+  int i;
+  int mnum=0;
+ n= INT_MAX;
+sum1=n*(n-1)/2
+for(i=0;i<n;i++)
+{sum2=sum+a[i];}
+mnum=sum1-sum2;
+return mnum;
+}
+
+int main()
+{ array=[1,2,3,4,6,7]
+mnum=Missing number(array)
+cout<<"Missing Number is "<< mnum;
+return 0;
+]
